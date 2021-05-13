a884dc4390c9c7f3d3ef6f52a9ce25f774900385
GAPtoothWONDER <83780450+GAPtoothWONDER@users.noreply.github.com>
2021-05-13 13:32:38 -0400

    Add files via upload

--- maps/Route29.asm
+++ maps/Route29.asm
@@ -150,7 +150,7 @@ Route29CooltrainerMScript:
 	opentext
 	checktime DAY
 	iftrue .day_morn
-	checktime NITE
+	checktime EVE | NITE
 	iftrue .nite
 .day_morn
 	writetext Route29CooltrainerMText_WaitingForNight
@@ -326,8 +326,9 @@ Route29CooltrainerMText_WaitingForDay: ; unreferenced
 	text "I'm waiting for"
 	line "#MON that"
 
-	para "appear only in the"
-	line "daytime."
+	para "appear only in"
+	line "the evening or"
+	cont "at night."
 	done
 
 Route29CooltrainerMText_WaitingForNight:
